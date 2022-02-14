fd9987b1b69b6fc6309089a4a4a182bda9deb499
Andrew Theiss <andrew.theiss@gmail.com>
2022-02-14 13:54:27 -0800

    Refactor event codes, (in progress)

--- src/input.js
+++ src/input.js
@@ -1,31 +1,54 @@
 export default class InputHandler{
 
   constructor(player){
+    this.keys = {
+      "shift" : 16,
+      "left" : 37,
+      "right" : 39,
+      "z" : 90
+    };
+    this.keyDown = {
+      shift : null
+    }
     document.addEventListener('keydown', (event)=>{
-      //alert(event.keyCode);
-      if(event.keyCode==16)//shift
-      {
-        player.dash();
-      }
 
-      if(event.keyCode==37)//left
-      {
-        player.moveLeft();
-      }
-      if(event.keyCode==39)//right
-      {
-        player.moveRight();
-      }
-      if(event.keyCode==90)//z
-      {
-        player.jump();
-      }
+      switch (event.keyCode) {
+        case keys.shift:
+          this.keyDown.shift = true;
+          player.dash();
+          break;
 
+        case keys.left:
+          player.moveLeft();
+          break;
 
+        case keys.right:
+          player.moveRight();
+          break;
 
+        case keys.jump:
+          player.jump();
+          break;
+
+        default:
+          break;
+      }
     });
     document.addEventListener('keyup', (event)=>{
+      switch (event.keyCode) {
+        case keys.shift:
+          this.keyDown.shift = false;
+          player.dash();
+          break;
 
+        case keys.left:
+        case keys.right:
+          player.stopHorizontal();
+          break;
+
+        default:
+          break;
+      }
       if(event.keyCode==37)//left
       {
         if(player.velX<0)
@@ -43,3 +66,8 @@ export default class InputHandler{
     });
   }
 }
+
+let input = new InputHandler(null);
+document.addEventListener('keydown', (event)=>{
+console.log(event.keyCode);
+});
