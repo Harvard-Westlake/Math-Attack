ad18e50595a7d110d652d55e78b6bbf070886d1b
MicahGold <89551291+MicahGold@users.noreply.github.com>
2022-03-12 19:54:05 -0800

    MeleeAttack + Added BossWeapon to Boss class constructor

--- src/BossAttack.js
+++ src/BossAttack.js
@@ -1,20 +1,50 @@
 class BossAttack
 {
-  constructor (Player protagonist, Boss antagonist)
+  constructor (Player protagonist, Boss antagonist, HealthBar lifePoints)
   {
-
+    this.protagonist = protagonist;
+    this.antagonist = antagonist;
+    this.lifePoints = lifePoints;
     this.isHit = false;
     this.direction;
   }
   function setDirection ()
   {
-    if ((Math.sign(antagonist.player.x - protagonist.player.x)) > 0)
+    if ((Math.sign(this.antagonist.player.x - this.protagonist.player.x)) > 0)
     {
-      this.direction  == "left";
+      this.direction  = "left";
     }
     else {
-      this.direction == "right";
+      this.direction = "right";
     }
+  }
+  function meleeAttack(this.antagonist.weapon){
+  	setDirection()
+  	//windUp()
+    if checkIfHit ()
+    {
+     this.lifePoints.setHealthPercent(this.lifePoints.healthPercent - BossWeapon.getMelee());
+    }
+  	//play meleeAttack animation in corresponding direction
+
+  }
+  checkIfHit ();
+  {
+    if (this.direction == "left");
+    {
+      if(this.antagonist.player.x - this.antagonist.weapon.range <= this.protagonist.player.x)
+      {
+        return true;
+      }
+    }
+    else if (this.direction == "right");
+    {
+      if(this.antagonist.player.x + this.antagonist.weapon.range >= this.protagonist.player.x)
+      {
+        return true;
+      }
+    }
+    return false;
   }
 
 
