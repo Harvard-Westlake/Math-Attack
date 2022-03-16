80ba7f6c9862044af9e839c7950c5ecf1b94f04a
Alex-Shane <ashane1230@gmail.com>
2022-03-15 19:18:36 -0700

    checkForCollision() working

--- src/collision.js
+++ src/collision.js
@@ -19,26 +19,26 @@ export default class collision{
 
   checkForCollision()
   {
-    let xCollision = false;
-    let xWindow1 = this.obj.x + this.obj.width;
-    let xWindow2 = thisobj2.x + this.obj2.width;
-    if (this.obj2.x > this.obj.x && this.obj2.x < this.xWindow1)
+    this.xCollision = false;
+    this.xWindow1 = this.obj.x + this.obj.width;
+    this.xWindow2 = this.obj2.x + this.obj2.width;
+    if (this.obj2.x >= this.obj.x && this.obj2.x <= this.xWindow1)
     {
       this.xCollision = true;
     }
-    if (this.xWindow2 > this.obj.x && this.xWindow2 < this.xWindow1)
+    if (this.xWindow2 >= this.obj.x && this.xWindow2 <= this.xWindow1)
     {
       this.xCollision = true;
     }
 
-    let yCollision = false;
-    let yWindow1 = this.obj.y + this.obj.height;
-    let yWindow2 = thisobj2.y + this.obj2.height;
-    if (this.obj2.y > this.obj.y && this.obj2.y < this.yWindow1)
+    this.yCollision = false;
+    this.yWindow1 = this.obj.y + this.obj.height;
+    this.yWindow2 = this.obj2.y + this.obj2.height;
+    if (this.obj2.y >= this.obj.y && this.obj2.y <= this.yWindow1)
     {
       this.yCollision = true;
     }
-    if (this.yWindow2 > this.obj.y && this.yWindow2 < this.yWindow1)
+    if (this.yWindow2 >= this.obj.y && this.yWindow2 <= this.yWindow1)
     {
       this.yCollision = true;
     }
@@ -49,7 +49,5 @@ export default class collision{
     }
     return false;
   }
-
-
 
 }
