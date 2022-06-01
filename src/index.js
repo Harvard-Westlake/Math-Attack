db7a167814e439e1bbd9da51410d0cd66ee62484
Aaron Cho <aars.cho10@gmail.com>
2022-05-31 22:20:56 -0700

    Invincibility frames longer
    Longer i-frames (timeout) so that the boss does not register two hearts of damage when the player is idling. It ain't much but it's honest work (also sifting through lots of weird code and many classes).

--- src/index.js
+++ src/index.js
@@ -277,7 +277,7 @@ class Game {
         this.bossPlayerCollisionTimeout = true;
         setTimeout(() => {
           this.bossPlayerCollisionTimeout = false;
-        }, 500);
+        }, 800);
         }
       }
   }
